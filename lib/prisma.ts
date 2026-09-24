608570010ccc3d125e3d5c537c80bbca0ac262e8
Renato Silva <dominguesrds@fab.mil.br>
2026-09-23 23:06:28 -0300

    fix: padronizar DATABASE_URL no schema prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
 const connectionString =
-  process.env.STORAGE_URL ||
   process.env.DATABASE_URL ||
+  process.env.STORAGE_URL ||
   process.env.POSTGRES_PRISMA_URL ||
   process.env.POSTGRES_URL ||
   process.env.STORAGE_URL_NON_POOLING;
